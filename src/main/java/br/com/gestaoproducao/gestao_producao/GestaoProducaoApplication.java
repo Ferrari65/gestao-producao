199b7ed9996a6ff334bf04a0d3275a40c10da266
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-10-16 21:18:02 -0300

    INICIALIZACAO DO PROJETO]

---
+++ src/main/java/br/com/gestaoproducao/gestao_producao/GestaoProducaoApplication.java
@@ -0,0 +1,14 @@
+package br.com.gestaoproducao.gestao_producao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProducaoApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/br/com/gestaoproducao/gestao_producao/GestaoProducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaoproducao.gestao_producao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoProducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
